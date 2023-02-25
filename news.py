139000b8c6ed864a55f7e884235e7a8619531bbc
barsik132 <60780835+barsik132@users.noreply.github.com>
2023-02-25 14:29:29 -0700

    Update news.py

--- news.py
+++ news.py
@@ -3,10 +3,10 @@ from telethon import errors
 import asyncio
 
 # ----
-api_id = "22176825"
+api_id = 22176825
 api_hash = "69f074b42d7e6ebb93230e8bbcfbe3e3"
 # ----
-channels = ['@cosplay_nudes' , "@coospart"  , "@Cosplay_18s" , "@fgfgdfgfdgdfg" ]# откуда
+channels = ['@cosplay_nudes' , "@coospart"  , "@Cosplay_18s" , "@fgfgdfgfdgdfg" , "@anicosplay"]# откуда
 my_channel = '@cosplay_patreon'  # куда
 # -----
 KEYS = {
@@ -20,49 +20,6 @@ tags = ''
 # ----
 with TelegramClient('myApp13', api_id, api_hash) as client:
     print("～Activated～")
-
-    @client.on(events.NewMessage(chats=channels))
-    async def Messages(event):
-        if not [element for element in Bad_Keys
-                if event.raw_text.lower().__contains__(element)]:
-            text = event.raw_text
-            for i in KEYS:
-                text = re.sub(i, KEYS[i], text)
-            if not event.grouped_id\
-                    and not event.message.forward:
-                try:
-                    await client.send_message(
-                        entity=my_channel,
-                        file=event.message.media,
-                        message=text + tags,
-                        parse_mode='md',
-                        link_preview=False)
-                except errors.FloodWaitError as e:
-                    print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
-                    await asyncio.sleep(e.seconds)
-                except Exception as e:
-                    print('[!] Ошибка', e)
-            elif event.message.text and not event.message.media\
-                and not event.message.forward\
-                    and not event.grouped_id:
-                try:
-                    await client.send_message(
-                        entity=my_channel,
-                        message=text + tags,
-                        parse_mode='md',
-                        link_preview=False)
-                except errors.FloodWaitError as e:
-                    print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
-                    await asyncio.sleep(e.seconds)
-                except Exception as e:
-                    print('[!] Ошибка', e)
-            elif event.message.forward:
-                try:
-                    await event.message.forward_to(my_channel)
-                except errors.FloodWaitError as e:
-                    print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
-                except Exception as e:
-                    print('[!] Ошибка', e)
 
     @client.on(events.Album(chats=channels))
     async def Album(event):
